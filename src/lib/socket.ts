d45f54a9bb39bde33dbec13b24c845bf6d7215b7
Sanju <psinghrajput907@gmail.com>
2025-09-07 21:21:29 +0530

    updated

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -5,7 +5,7 @@ let socket: Socket | undefined;
 
 export const getSocket = (): Socket => {
     if (!socket) {
-        socket = io(process.env.BACKEND_URL || "http://localhost:4521",{
+        socket = io(process.env.BACKEND_URL || "https://socket-kcih.onrender.com",{
             autoConnect: true,
             reconnection: true,
             reconnectionAttempts: 5,
