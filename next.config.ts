1fdaa06f42828f849b766fb12c22961263c63bce
Sanju <psinghrajput907@gmail.com>
2026-04-02 11:01:33 +0530

    fix: stabilize vercel build config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,9 @@
 import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    eslint: {
-        // This will now be recognized correctly
-        ignoreDuringBuilds: true,
-    },
     typescript: {
         ignoreBuildErrors: true,
     },
-    // Add other config here
 };
 
 export default nextConfig;
